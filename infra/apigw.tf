dd0fc9b31854b04990e3de4ff8e2507e7a6f3c2d
Georgi Stavrev <georgi@insurify.com>
2021-08-06 19:09:48 +0300

    adds lcrud logic for tasks (#3)

--- infra/apigw.tf
+++ infra/apigw.tf
@@ -1,3 +1,4 @@
+// Route /{organisationId}
 module "organisationId_param" {
   source             = "git@github.com:moggiez/terraform-modules.git//lambda_gateway"
   api                = aws_api_gateway_rest_api._
@@ -15,6 +16,7 @@ module "organisationId_param_cors" {
   api_resource_id = module.organisationId_param.api_resource.id
 }
 
+// Route /{organisationId}/jobs
 module "jobs_part" {
   source             = "git@github.com:moggiez/terraform-modules.git//lambda_gateway"
   api                = aws_api_gateway_rest_api._
@@ -33,6 +35,7 @@ module "jobs_part_cors" {
   api_resource_id = module.jobs_part.api_resource.id
 }
 
+// Route /{organisationId}/jobs/{jobId}
 module "jobId_param" {
   source             = "git@github.com:moggiez/terraform-modules.git//lambda_gateway"
   api                = aws_api_gateway_rest_api._
@@ -42,11 +45,47 @@ module "jobId_param" {
   resource_path_part = "{jobId}"
 
   authorizer = local.authorizer
-
 }
 
 module "jobId_param_cors" {
   source          = "git@github.com:moggiez/terraform-modules.git//api_gateway_enable_cors"
   api_id          = aws_api_gateway_rest_api._.id
   api_resource_id = module.jobId_param.api_resource.id
+}
+
+// Route /{organisationId}/jobs/{jobId}/tasks
+module "tasks_part" {
+  source             = "git@github.com:moggiez/terraform-modules.git//lambda_gateway"
+  api                = aws_api_gateway_rest_api._
+  parent_resource    = module.jobId_param.api_resource
+  lambda             = module.api_lambda.lambda
+  http_methods       = local.http_methods
+  resource_path_part = "tasks"
+
+  authorizer = local.authorizer
+
+}
+
+module "tasks_part_cors" {
+  source          = "git@github.com:moggiez/terraform-modules.git//api_gateway_enable_cors"
+  api_id          = aws_api_gateway_rest_api._.id
+  api_resource_id = module.tasks_part.api_resource.id
+}
+
+// Route /{organisationId}/jobs/{jobId}/tasks/{taskId}
+module "taskId_param" {
+  source             = "git@github.com:moggiez/terraform-modules.git//lambda_gateway"
+  api                = aws_api_gateway_rest_api._
+  parent_resource    = module.tasks_part.api_resource
+  lambda             = module.api_lambda.lambda
+  http_methods       = local.http_methods
+  resource_path_part = "{taskId}"
+
+  authorizer = local.authorizer
+}
+
+module "taskId_param_cors" {
+  source          = "git@github.com:moggiez/terraform-modules.git//api_gateway_enable_cors"
+  api_id          = aws_api_gateway_rest_api._.id
+  api_resource_id = module.taskId_param.api_resource.id
 }
